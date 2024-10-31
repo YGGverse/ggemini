b73a9388eeca89eb606d5b604625d08536337aa3
yggverse <yggverse@project>
2024-10-31 03:17:57 +0200

    update comment

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-//! Client API to interact Server for
+//! Client API to interact Server using
 //! [Gemini protocol](https://geminiprotocol.net/docs/protocol-specification.gmi)
 
 pub mod response;
