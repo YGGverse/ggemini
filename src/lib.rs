54f2b81475506094a51ff9f2f71b05d582167074
yggverse <yggverse@project>
2025-01-13 21:23:52 +0200

    add versions debug API

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,11 @@ pub use client::Client;
 // Global defaults
 
 pub const DEFAULT_PORT: u16 = 1965;
+
+// Debug
+
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub const VERSION_MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
+pub const VERSION_MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
+pub const VERSION_PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
